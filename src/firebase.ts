a3436a12c32f4aa297f0d0df076a9b351a78f0c1
Tu Nombre <roberto@mistatas.com>
2025-06-05 17:39:43 -0400

    fix: correct firebase exports

---
+++ src/firebase.ts
@@ -0,0 +1,17 @@
+import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: "rutas-soporte-ti.firebaseapp.com",
+  projectId: "rutas-soporte-ti",
+  storageBucket: "rutas-soporte-ti.appspot.com",
+  messagingSenderId: "571387968108",
+  appId: "1:571387968108:web:1cd279ff67f36e767cb649"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
